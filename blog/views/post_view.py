2a3d44734fa73f4060c6b886a3abbcdff7e04286
Gustavo <gustavoburgerf@gmail.com>
2025-08-05 20:46:05 -0300

    ajustando os templates

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -3,10 +3,10 @@ from blog.models import Post
 
 class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"  # <-- caminho recomendado
+    template_name = "index.html"
     context_object_name = "post_list"
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = "post_detail.html"  # <-- ajuste se necessário
+    template_name = "post_detail.html"
     context_object_name = "post"
