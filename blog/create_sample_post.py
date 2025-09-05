0a4015be78461720c547436b2155c883342c377e
Gustavo <gustavoburgerf@gmail.com>
2025-09-05 20:35:20 -0300

    criar o usuario automaticamente

--- blog/create_sample_post.py
+++ blog/create_sample_post.py
@@ -1,33 +1,21 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django.setup()
-
-from blog.models import Post
 from django.contrib.auth.models import User
+from .models import Post
 
-user, created = User.objects.get_or_create(
-    username='admin',
-    defaults={'email': 'admin@example.com', 'is_staff': True, 'is_superuser': True}
-)
-if created:
-    user.set_password('senha123')
-    user.save()
-    print("Superusuário criado!")
-else:
-    print("O superusuário já existia.")
+def create_sample_post(user=None):
+    # Se user não for passado, pega o admin
+    if user is None:
+        user = User.objects.filter(username='admin').first()
 
-post, created = Post.objects.get_or_create(
-    slug='second-post',
-    defaults={
-        'title': 'Second Post',
-        'content': 'Conteúdo de exemplo para o segundo post.',
-        'author': user,
-        'status': 1,
-    }
-)
-if created:
-    print("Post criado com sucesso!")
-else:
-    print("O post já existia.")
+    post, created = Post.objects.get_or_create(
+        slug='second-post',
+        defaults={
+            'title': 'Second Post',
+            'content': 'Conteúdo de exemplo para o segundo post.',
+            'author': user,
+            'status': 1,
+        }
+    )
+    if created:
+        print("Post público criado automaticamente!")
+    else:
+        print("O post já existia.")
