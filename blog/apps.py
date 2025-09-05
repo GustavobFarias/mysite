8eb15c0db2e432abcad1d3b8c0e3cada0e15bd7c
Gustavo <gustavoburgerf@gmail.com>
2025-09-05 20:32:10 -0300

    criar o usuario automaticamente

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+    def ready(self):
+        from .create_sample_post import create_sample_post
+        create_sample_post()
---
+++ blog/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Post
+from .create_sample_post import create_sample_post
+
+@receiver(post_migrate)
+def create_superuser_and_post(sender, **kwargs):
+    # Cria superusuário se não existir
+    user, created = User.objects.get_or_create(
+        username='admin',
+        defaults={'email': 'admin@example.com', 'is_staff': True, 'is_superuser': True}
+    )
+    if created:
+        user.set_password('senha123')
+        user.save()
+        print("Superusuário criado automaticamente!")
+
+    # Cria post usando sua função existente
+    create_sample_post(user)
